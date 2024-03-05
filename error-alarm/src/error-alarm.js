49a1fbbaf719cb7ef4cc8062d12c965bee8c1f8a
pndiaz <pablondiaz12@gmail.com>
2024-03-05 10:24:24 -0300

    error-alarm minor refactor

--- error-alarm/src/error-alarm.js
+++ error-alarm/src/error-alarm.js
@@ -1,34 +1,40 @@
+const fs = require('fs');
 
 class ErrorLogger {
 
     async logError(error) {
         // This function is called each time there is an error and appends the error to the end of the log file.
+        // The logic of checking the error frecuency shouldn't be here just to not affect the save error logic.
     }
 
     async sendEmailErrorNotification() { }
 
     /**
      * Cron job that allow us read from the text file and decide if it's necessary to send an email notification
+     * We can define to run this once a minute and skip checking if the email was sent in the past minute
      */
     async checkErrorFrecuency() {
 
         // Read errors from file
-        // Ideally get the last 10 errors to avoid read the entire file. We need some other structure for that.
-        const errors = []
+        // Ideally we need another structure to read only the lastest errors or keep a memory array to avoid reading the file every time.
+        const errors = fs.readFileSync('error.log');
         const currentTimestamp = Date.now();
+
 
         //Filter only the errors in the last minute
         const errorsInLastMinute = errors.filter((error) => {
 
             // Extract timestamp from the log in someway
             const logTimestamp = new Date(error.split('-')[0]).getTime();
+
             return currentTimestamp - logTimestamp < 60000;
         });
 
-        if (errorsInLastMinute > 10) {
+
+        if (errorsInLastMinute.length > 10) {
+            // Send email notification
             if (currentTimestamp - lastNotificationTimestamp > 60000) {
-               
-                // Send email notification
+                
                 this.sendEmailErrorNotification();
 
                 // Update email timestamp
@@ -36,7 +42,7 @@ class ErrorLogger {
             }
         }
     }
-}
+};
 
 
 module.exports = { ErrorLogger }
